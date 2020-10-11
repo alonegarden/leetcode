184d7575d084a973550625d7e3589fa5741b809f
carlos <1447675994@qq.com>
2020-10-12 00:57:44 +0800

    分割等和子集

---
+++ partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+"""
+# https://leetcode-cn.com/problems/partition-equal-subset-sum/
+# 416. 分割等和子集
+
+给定一个只包含正整数的非空数组。是否可以将这个数组分割成两个子集，使得两个子集的元素和相等。
+
+注意:
+
+每个数组中的元素不会超过 100
+数组的大小不会超过 200
+示例 1:
+
+输入: [1, 5, 11, 5]
+
+输出: true
+
+解释: 数组可以分割成 [1, 5, 5] 和 [11].
+
+
+
+思路小结：
+
+
+
+
+
+"""   
+"""
+要定义链表 才可以执行
+"""
+
+def canPartition(nums):
+    
+    return 
+
+
+if __name__ == '__main__':
+    head =  [1, 5, 11, 5]
+    print(canPartition(head))
+
+
+
+
